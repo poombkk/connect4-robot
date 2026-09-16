abf861d74a8013da1e62725a27f755298eee6f3e
poombkk <poombkk.poom@gmail.com>
2026-09-16 21:06:42 +0700

    Add drop piece system

--- board.py
+++ board.py
@@ -9,6 +9,7 @@ def create_board():
 
 
 def print_board(board):
+    print("  0 1 2 3 4 5 6")
     for r in range(5,-1,-1) :
         line = str(r) + " "
         for c in range(7):
@@ -16,7 +17,24 @@ def print_board(board):
             symbol = "." if cell == EMPTY else ("R" if cell == RED else "Y")
             line = line + symbol + " "
         print(line)
+    
+def is_valid_move(board, col):
+    return board[ ROWS - 1 ][col] == EMPTY
+
+def get_next_open_row(board, col):
+    for r in range(ROWS):
+        if board[r][col] == EMPTY:
+            return r
+    return None
+
+def drop_piece(board, col, player):
+    if not is_valid_move(board, col):
+        return -1
+
+    r = get_next_open_row(board, col)
+    board[r][col] = player
+    return r 
 
 board = create_board()
-print("  0 1 2 3 4 5 6")
+drop_piece(board, 0, YELLOW)
 print(print_board(board))
