dd387484d9450affe5f0322034db277dc12b1805
poombkk <poombkk.poom@gmail.com>
2026-09-20 15:29:22 +0700

    add function winning move

--- board.py
+++ board.py
@@ -35,6 +35,51 @@ def drop_piece(board, col, player):
     board[r][col] = player
     return r 
 
+def winning_move(board, player):
+# ---- แนวนอน
+    for r in range(ROWS):
+        for c in range(COLS - 2): 
+            if(
+                 board[r][c] == player and board[r][c+1] == player
+                and board[r][c+2] == player and board[r][c+3] == player
+            ):
+                return True
+# ---- แนวตั้ง
+    for r in range(ROWS - 3):
+        for c in range(COLS):
+            if(
+                 board[r][c] == player and board[r+1][c] == player 
+                and board[r+2][c] == player and board[r+3][c] == player
+            ):
+                return True
+# ---- ทแยงขึ้น
+    for r in range(ROWS - 3):
+        for c in range(COLS):
+            if(
+                 board[r][c] == player and board[r+1][c+1] == player 
+                and board[r+2][c+2] == player and board[r+3][c+3] == player
+            ):
+                return True
+# ---- ทแยงลง
+    for r in range(ROWS -1, 2, -1):
+        for c in range(COLS):
+            if(
+                 board[r][c] == player and board[r-1][c+1] == player 
+                and board[r-2][c+2] == player and board[r-3][c+3] == player
+            ):
+                return True
+    return False
+
 board = create_board()
+drop_piece(board, 3, YELLOW)
+drop_piece(board, 2, RED)
+drop_piece(board, 2, YELLOW)
+drop_piece(board, 1, RED)
+drop_piece(board, 1, RED)
+drop_piece(board, 1, YELLOW)
+drop_piece(board, 0, RED)
+drop_piece(board, 0, RED)
+drop_piece(board, 0, RED)
 drop_piece(board, 0, YELLOW)
-print(print_board(board))
+print_board(board)
+print(winning_move(board, YELLOW))
